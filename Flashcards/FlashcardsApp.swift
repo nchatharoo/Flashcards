2342583f8d61e5199b1ae2ff54fc33ca85cc2df9
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-11-12 15:05:04 +0100

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Nadheer on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashcardsTests/FlashcardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsTests.swift
+//  FlashcardsTests
+//
+//  Created by Nadheer on 12/11/2024.
+//
+
+import Testing
+@testable import Flashcards
+
+struct FlashcardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
