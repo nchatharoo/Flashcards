dc13a91fcdfe6ab490e6b0eb64ba2323f6a37bc9
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-11-13 10:25:36 +0100

    Use new colors

--- Flashcards/Views/OptionButton.swift
+++ Flashcards/Views/OptionButton.swift
@@ -19,7 +19,7 @@ struct OptionButton: View {
                 .frame(maxWidth: .infinity)
                 .padding()
                 .background(backgroundColor)
-                .foregroundColor(.white)
+                .foregroundColor(isSelected ? .white : Color(#colorLiteral(red: 0, green: 0.1550326347, blue: 0.1517512798, alpha: 1)))
                 .cornerRadius(10)
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
@@ -35,10 +35,15 @@ struct OptionButton: View {
         } else if isSelected {
             return .red
         }
-        return .blue
+        return Color(#colorLiteral(red: 0.9854046702, green: 0.7808517218, blue: 0.4496312737, alpha: 1))
     }
     
     private var borderColor: Color {
-        isSelected ? .white : .clear
+        if isCorrect {
+            return .green
+        } else if isSelected {
+            return .white
+        }
+        return .clear
     }
 }
